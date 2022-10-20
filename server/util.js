a4e43767459a92c84af6bc2b71636ecddff3154e
Shubham Panchal <panchalshubham@Shubhams-MacBook-Pro.local>
2022-10-20 08:37:18 +0530

    add use-case for macOS

--- server/util.js
+++ server/util.js
@@ -1,19 +1,37 @@
 const fs = require('fs');
+const os = require('os');
+const { exec } = require('child_process');
 const screenshot = require('desktop-screenshot');
 
-const filename = '/tmp/screenshot.jpg'
+const extension = '.jpg';
+const filename = 'screenshot' + extension;
+const filepath = os.tmpdir() + '/' + filename;
 module.exports.screenshot = () => new Promise((resolve, reject) => {
-    screenshot(filename, function(error, complete) {
-        if (error) {
-            reject(error);
-        } else if (!complete) {
-            reject('Failed to capture screenshot');
-        } else {
-            try {
-                resolve(fs.readFileSync(filename));
-            } catch (err) {
+    if (os.type() === 'Darwin') {
+        exec('screencapture -x ' + filepath, (err, stdout, stderr) => {
+            if (err) {
                 reject(err);
+            } else {
+                try {
+                    resolve(fs.readFileSync(filepath));
+                } catch (err) {
+                    reject(err);
+                }
             }
-        }
-    });
+        });
+    } else {
+        screenshot(filename, function(error, complete) {
+            if (error) {
+                reject(error);
+            } else if (!complete) {
+                reject('Failed to capture screenshot');
+            } else {
+                try {
+                    resolve(fs.readFileSync(filepath));
+                } catch (err) {
+                    reject(err);
+                }
+            }
+        });
+    }
 });
